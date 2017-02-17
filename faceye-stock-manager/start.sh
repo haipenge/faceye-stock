d82eee42d1cd86f10d8b4c82f39179b1089920a7
haipenge@gmail.com <haipenge@gmail.com>
2017-02-17 18:49:52 +0800

    修改启动脚本

--- faceye-stock-manager/start.sh
+++ faceye-stock-manager/start.sh
@@ -2,4 +2,4 @@ ROOT=$(dirname $(cd "$(dirname "$0")";pwd))
 cd $ROOT/faceye-stock-entity
 mvn clean compile package install -D maven.test.skip=true -P product
 cd $ROOT/faceye-stock-manager
-mvn clean compile jetty:run -D maven.test.skip=true
+mvn clean compile jetty:run -D maven.test.skip=true -D jetty.port=8089
