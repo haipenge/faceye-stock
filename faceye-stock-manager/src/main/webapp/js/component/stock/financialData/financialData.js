a149b7a8ca8dadc5f6c54aad47cdbf3a0c3d191d
haipenge@gmail.com <haipenge@gmail.com>
2017-02-24 18:57:51 +0800

    分页获取数据

--- faceye-stock-manager/src/main/webapp/js/component/stock/financialData/financialData.js
+++ faceye-stock-manager/src/main/webapp/js/component/stock/financialData/financialData.js
@@ -70,10 +70,45 @@ var FinancialData = {
 			}
 		});
 	},
+	showTooltip : function(x, y, contents) {
+		$("<div id='tooltip'>" + contents + "</div>").css({
+			position : "absolute",
+			display : "none",
+			top : y + 5,
+			left : x + 5,
+			border : "1px solid #000",
+			padding : "5px",
+			'color' : '#fff',
+			'border-radius' : '2px',
+			'font-size' : '11px',
+			"background-color" : "#000",
+			opacity : 0.80
+		}).appendTo("body").fadeIn(200);
+	},
+	/**
+	 * 绑定气泡
+	 */
+	bindPlothover : function(el) {
+		$(el).bind("plothover", function(event, pos, item) {
+			var str = "(" + pos.x.toFixed(2) + ", " + pos.y.toFixed(2) + ")";
+			if (item) {
+				if (previousPoint != item.dataIndex) {
+					previousPoint = item.dataIndex;
+					$("#tooltip").remove();
+					var x = item.datapoint[0].toFixed(2), y = item.datapoint[1].toFixed(2);
+					showTooltip(item.pageX, item.pageY, item.series.label + " of " + x + " = " + y);
+				}
+			} else {
+				$("#tooltip").remove();
+				previousPoint = null;
+			}
+		});
+	},
 	/**
 	 * 对某一会计科目的绘图统计
 	 */
 	chartsQuery : function(stockId, accountingSubjectId, el) {
+		FinancialData.bindPlothover(el);
 		$.ajax({
 			url : '/stock/financialData/chartsQuery',
 			type : 'post',
@@ -82,17 +117,17 @@ var FinancialData = {
 				accountingSubjectId : accountingSubjectId
 			},
 			success : function(data, textStatus, xhr) {
-				var datas=[];
-				var dates=[];
-				var count=1;
-				for(var i=0;i<data.length;i++){
-					var record=data[i];
-					var newDate=new Date();
+				var datas = [];
+				var dates = [];
+				var count = 1;
+				for (var i = 0; i < data.length; i++) {
+					var record = data[i];
+					var newDate = new Date();
 					newDate.setTime(record.date);
-					var dateStr=newDate.format('yyyy-MM-dd');
-//					var array=[dateStr,record.data];
-					var arr1=[count,record.data/100000];
-					var arr2=[count,dateStr];
+					var dateStr = newDate.format('yyyy-MM-dd');
+					// var array=[dateStr,record.data];
+					var arr1 = [ count, record.data / 100000 ];
+					var arr2 = [ count, dateStr ];
 					datas.push(arr1);
 					dates.push(arr2);
 					count++;
@@ -134,8 +169,7 @@ var FinancialData = {
 					xaxis : {
 						ticks : dates,
 						tickDecimals : 0
-					}
-					,
+					},
 					yaxis : {
 						ticks : 5,
 						tickDecimals : 0
