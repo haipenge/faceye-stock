be02a5d8192d859d80480f2edf06a1f2bede67ca
haipenge <haipenge@gmail.com>
2017-07-11 10:42:18 +0800

    add start scripts for cloud env

--- faceye-stock-manager/start.sh
+++ faceye-stock-manager/start.sh
@@ -1,9 +1,14 @@
 ROOT=$(dirname $(cd "$(dirname "$0")";pwd))
-rm -rf /data/logs/faceye-stock-manager/*
+rm -rf /tmp/logs/faceye-stock-manager/*
 cd $ROOT
 git pull
 echo `ps aux|grep jetty |grep -v grep  | awk '{print $2}'|xargs kill -9`
+build_env='product'
+if [[ -n $1 ]];then
+	build_env=$1
+fi
+echo $build_env
 cd $ROOT/faceye-stock-entity
 mvn clean compile package install -D maven.test.skip=true -P product
 cd $ROOT/faceye-stock-manager
-mvn clean compile jetty:run -D maven.test.skip=true -D jetty.port=8089 -P product &
+mvn clean compile jetty:run -D maven.test.skip=true -D jetty.port=8089 -P $build_env &
